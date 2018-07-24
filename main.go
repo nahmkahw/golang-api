ec281effd1b53ac48df0f7aa74b707bb35eebaf0
nahmkahw <nahmkahw@gmail.com>
2018-07-24 14:21:36 +0700

    05-group-routes

--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	router.Use(gin.Logger())
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	router.Use(gin.Recovery())
+
 	// Simple group: public
 	routerpublic := router.Group("/public")
 	{
